efea805805213fb89ccd6fba0e262a6e354944e4
kentrell <kentrellcorey@hotmail.com>
2020-05-06 16:57:17 -0400

    Both Exercises

---
+++ ConditionsExer2/script.js
@@ -0,0 +1,14 @@
+let name1 = "Friza" 
+let name2 = "Vageta"
+let name3 = "Gohan"
+    if (name1.length < name2.length) {
+        
+    console.log("Vageta has the longest name");
+}   if (name1.length === name3.length) {
+
+    console.log("Friza and Gohan tie for the longest name")
+}
+
+
+
+
---
+++ LoopsExer2/script.js
@@ -0,0 +1,7 @@
+let system = "XBOXPS";
+let game = "COD8";
+    if (system.length > game.length) 
+console.log(system.padStart(10));
+
+console.log(game.padStart(10));
+    
